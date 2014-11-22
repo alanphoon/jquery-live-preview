815439636aa2f16a79cab2a36862ecd10c079578
Mike Dobozy <dobozysaurus@gmail.com>
2014-11-22 08:36:05 -0500

    Added data-preview-url option on the link, which allows the preview url to differ from the link url.

--- js/jquery-live-preview.js
+++ js/jquery-live-preview.js
@@ -7,7 +7,8 @@ Licensed under GNU GENERAL PUBLIC LICENSE
 ***********************************************/
 (function($) {
   $.fn.extend({
-     livePreview: function(options){
+     livePreview: function(options) {
+         
          var defaults = {
              trigger: 'hover',
              targetWidth : 1000,
@@ -16,7 +17,7 @@ Licensed under GNU GENERAL PUBLIC LICENSE
              viewHeight: 200,
              position: 'right',
              positionOffset: 50,
-         }
+         };
 
          var options = $.extend(defaults, options);
          //calculate appropriate scaling based on width.
@@ -34,12 +35,44 @@ Licensed under GNU GENERAL PUBLIC LICENSE
              else
                  scale_f = scale_h;
          }
+         
+         var showPreview = function(event) {
+             var triggerType = event.data.triggerType;
+             var obj = event.data.target;
+             var href = event.data.href;
+             var s = event.data.scale;
+             
+             if( (triggerType == 'click') && ($('#' + preview_id).length == 0) ) {
+                 event.preventDefault();
+             }
+
+             var currentPos = options.position;
+              if(obj.attr("data-position"))
+                 currentPos = obj.attr("data-position");
+
+             var currentOffset = options.positionOffset;
+             if(obj.attr("data-positionOffset"))
+                 currentOffset = obj.attr("data-positionOffset");
+
+             if(obj.attr("data-scale"))
+                 s = obj.attr("data-scale");
+
+             var pos = $(this).offset();
+             var width = $(this).width();
+             var leftpos = pos.left + width + currentOffset;
+             if(currentPos == 'left')
+                 leftpos = pos.left - options.viewWidth - currentOffset;
+             var toppos = pos.top - (options.viewHeight/2);
+             //hover on 
+             $('body').append('<div id="livepreview_dialog" class="' + currentPos + '" style="display:none; padding:0px; left: ' + leftpos + 'px; top:' + toppos + 'px; width: ' + options.viewWidth + 'px; height: ' + options.viewHeight + 'px"><div class="livepreview-container" style="overflow:hidden; width: ' + options.viewWidth + 'px; height: ' + options.viewHeight + 'px"><iframe id="livepreview_iframe" src="' + href + '" style="height:' + options.targetHeight + 'px; width:' + options.targetWidth + 'px;-moz-transform: scale('+ s + ');-moz-transform-origin: 0 0;-o-transform: scale('+ s + ');-o-transform-origin: 0 0;-webkit-transform: scale('+ s + ');-webkit-transform-origin: 0 0;"></iframe></div></div>');
+             $('#' + preview_id).fadeIn(100);
+         };
 
          return this.each(function() {
             var o = options;
             var s = scale_f;
             var obj = $(this);
-            var href = $(this).attr("href");
+            var href = obj.attr("data-preview-url") || obj.attr("href");
             var triggerType = options.trigger;
 
             if(obj.attr("data-trigger")) {
@@ -52,42 +85,14 @@ Licensed under GNU GENERAL PUBLIC LICENSE
                     $('#' + preview_id).remove();
                 });
             }
-
-            obj.on(triggerType, function() {
-
-                if( (triggerType == 'click') && ($('#' + preview_id).length == 0) ) {
-                    event.preventDefault();
-                }
-
-                var currentPos = o.position;
-                 if(obj.attr("data-position"))
-                    currentPos = obj.attr("data-position");
-
-                var currentOffset = o.positionOffset;
-                if(obj.attr("data-positionOffset"))
-                    currentOffset = obj.attr("data-positionOffset");
-
-                if(obj.attr("data-scale"))
-                    s = obj.attr("data-scale");
-
-                var pos = $(this).offset();
-                var width = $(this).width();
-                var leftpos = pos.left + width + currentOffset;
-                if(currentPos == 'left')
-                    leftpos = pos.left - o.viewWidth - currentOffset;
-                var toppos = pos.top - (o.viewHeight/2);
-                //hover on 
-                $('body').append('<div id="livepreview_dialog" class="' + currentPos + '" style="display:none; padding:0px; left: ' + leftpos + 'px; top:' + toppos + 'px; width: ' + o.viewWidth + 'px; height: ' + o.viewHeight + 'px"><div class="livepreview-container" style="overflow:hidden; width: ' + o.viewWidth + 'px; height: ' + o.viewHeight + 'px"><iframe id="livepreview_iframe" src="' + href + '" style="height:' + o.targetHeight + 'px; width:' + o.targetWidth + 'px;-moz-transform: scale('+ s + ');-moz-transform-origin: 0 0;-o-transform: scale('+ s + ');-o-transform-origin: 0 0;-webkit-transform: scale('+ s + ');-webkit-transform-origin: 0 0;"></iframe></div></div>');
-                $('#' + preview_id).fadeIn(100);
-
-            });
-
+            
+            obj.on(triggerType, null, { triggerType: triggerType, target: obj, href: href, scale: s }, showPreview);
             obj.on('mouseleave', function() {
                 $('#' + preview_id).remove();
             });
-            
 
          });
      }
   });
 })(jQuery);
+
